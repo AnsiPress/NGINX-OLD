f660bde35a8a6c2532cbf6a0f485044189f72b63
Mitesh Shah <Mr.Miteshah@gmail.com>
2017-04-24 11:49:56 +0530

    Updated Modules

--- build.sh
+++ build.sh
@@ -63,12 +63,21 @@ ppa_lib_echo "Downloading NGINX modules, please wait"
 mkdir ~/PPA/nginx/modules && cd ~/PPA/nginx/modules \
 || ppa_error "Unable to create ~/PPA/nginx/modules, exit status = " $?
 
-ppa_lib_echo "1/1 ngx_pagespeed"
+ppa_lib_echo "1/3 echo-nginx-module"
+git clone https://github.com/openresty/echo-nginx-module.git nginx-echo \
+|| ppa_error "Unable to clone echo-nginx-module repo, exit status = " $?
+
+ppa_lib_echo "2/3 lua-nginx-module"
+git clone https://github.com/openresty/lua-nginx-module.git nginx-lua \
+|| ppa_error "Unable to clone lua-nginx-module repo, exit status = " $?
+
+ppa_lib_echo "3/3 ngx_pagespeed"
 NPS_VERSION=1.11.33.4
-wget https://github.com/pagespeed/ngx_pagespeed/archive/v${NPS_VERSION}-beta.tar.gz
-tar -zxvf v${NPS_VERSION}-beta.tar.gz
-mv ngx_pagespeed-${NPS_VERSION}-beta  ngx_pagespeed
-rm v${NPS_VERSION}-beta.tar.gz
+wget https://github.com/pagespeed/ngx_pagespeed/archive/v${NPS_VERSION}-beta.tar.gz && \
+tar -zxvf v${NPS_VERSION}-beta.tar.gz && \
+mv ngx_pagespeed-${NPS_VERSION}-beta  ngx_pagespeed && \
+rm v${NPS_VERSION}-beta.tar.gz \
+|| ppa_error "Unable to clone ngx_pagespeed repo, exit status = " $?
 
 cd ngx_pagespeed
 wget -O psol.tar.gz https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz
